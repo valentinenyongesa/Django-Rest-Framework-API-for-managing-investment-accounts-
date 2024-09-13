88e9b57a064bc927b86a27a434f2415e2e9f7379
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-09-13 07:00:52 +0300

    Implementing custom permissions based on the type of account access

--- investment/views.py
+++ investment/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+# investment/views.py
+from rest_framework import viewsets, permissions
+from .models import InvestmentAccount, Transaction, AccountUser
+from .serializers import InvestmentAccountSerializer
+from .serializers import TransactionSerializer
+from .serializers import AccountUserSerializer
 
-# Create your views here.
+# ViewSet for the InvestmentAccount model.
+
+
+class InvestmentAccountViewSet(viewsets.ModelViewSet):
+    queryset = InvestmentAccount.objects.all()  # Fetch all investment accounts
+    serializer_class = InvestmentAccountSerializer  # Use defined serializer
+
+# ViewSet for the Transaction model.
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+    def get_permissions(self):
+        # Defines custom permissions based on the requirements.
+        return [permissions.IsAuthenticated()]
+
+# ViewSet for the AccountUser model.
+
+
+class AccountUserViewSet(viewsets.ModelViewSet):
+    queryset = AccountUser.objects.all()
+    serializer_class = AccountUserSerializer

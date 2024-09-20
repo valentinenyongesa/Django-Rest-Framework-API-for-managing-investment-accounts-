635d9c0778e91cd89068f25f45dbf45db22d60dc
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-09-20 11:13:49 +0300

    Modifying the admin.py to add users to group

--- investment/admin.py
+++ investment/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
-# Register your models here.
+class UserInline(admin.TabularInline):
+    model = Group.user_set.through
+    extra = 1  # Number of empty forms to display
+
+class GroupAdmin(admin.ModelAdmin):
+    inlines = [UserInline]
+
+# Unregister the default Group admin
+admin.site.unregister(Group)
+# Register the customized Group admin
+admin.site.register(Group, GroupAdmin)
--- investment/urls.py
+++ investment/urls.py
@@ -8,10 +8,10 @@ from .views import AccountUserViewSet
 # Sets up a DefaultRouter and registers the ViewSets with it.
 router = DefaultRouter()
 router.register(r'investment-accounts',
-                InvestmentAccountViewSet)  # Route for investment accounts
-router.register(r'transactions', TransactionViewSet)  # Route for transactions
+                InvestmentAccountViewSet, basename='investmentaccount')  # Route for investment accounts
+router.register(r'transactions', TransactionViewSet, basename='transaction')  # Route for transactions
 router.register(r'account-users',
-                AccountUserViewSet)  # Route for account-user relations
+                AccountUserViewSet, basename='accountuser')  # Route for account-user relations
 
 # Includes the router's URLs in the app's URL patterns.
 urlpatterns = [
